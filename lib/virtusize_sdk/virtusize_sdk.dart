46ead15e22a672e19f0dc03dece66efeb8f2684d
TheBaytur <bayturcodding@gmail.com>
2025-09-20 15:07:56 +0800

    Exporting all necessary components of the SDK

--- lib/virtusize_sdk/virtusize_sdk.dart
+++ lib/virtusize_sdk/virtusize_sdk.dart
@@ -1,5 +1,7 @@
 library virtusize_sdk;
 
+// Exporting all necessary components of the SDK.
+
 export 'src/recommendation.dart';
 export 'src/recommendation_calculator.dart';
 export 'src/size_category.dart';
